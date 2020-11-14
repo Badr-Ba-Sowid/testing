837c622ce02d70a40726c636479513bef6e735bc
Badr-Ba-Sowid <61441185+Badr-Ba-Sowid@users.noreply.github.com>
2020-11-14 14:20:35 +0300

    i added the grid listener

--- app/src/main/java/com/example/linearpractice/MainActivity.java
+++ app/src/main/java/com/example/linearpractice/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,6 +43,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent=new Intent(v.getContext(),Const.class);
                 startActivity(intent);
+            }
+        });
+
+        grid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext() , Grid.class));
             }
         });
 
